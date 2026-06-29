2127a645f47b27a4e74b09ddefc9f71f49aa4cdb
yogesh tomar <yogeshtomar970@gmail.com>
2026-06-29 17:45:13 +0530

    newsfeedstate.jsx update

--- src/component/NewsFeedState.jsx
+++ src/component/NewsFeedState.jsx
@@ -1,18 +1,12 @@
 import React from "react";
+import FeedLoader from "./FeedLoader";
 
 /**
  * NewsFeedState
  * Loading dots, error message with retry, empty message.
  */
 export default function NewsFeedState({ loading, error, empty, onRetry }) {
-  if (loading) return (
-    <div className="news-state-center">
-      <div className="news-loader">
-        <div /><div /><div />
-      </div>
-      <p>Loading news…</p>
-    </div>
-  );
+  if (loading) return <FeedLoader />;
 
   if (error) return (
     <div className="news-state-center">
